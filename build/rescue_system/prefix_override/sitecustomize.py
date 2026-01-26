22405fd017da18369848fef7046b6e4f0030647d
jh-lee01 <tg42008@gmail.com>
2026-01-26 12:02:20 +0000

    rebuild

--- build/rescue_system/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/home/hansung/miniconda3/envs/kroc':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/hansung/kroc/src/rescue_system/install/rescue_system'
